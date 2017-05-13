4fa85517e4ea7bb8f0ee7052ae8a9064dcb32942
Luiz Varela <luiz.lvarela@gmail.com>
2017-05-13 15:34:42 -0300

    Create todo and items controllers

---
+++ app/controllers/concerns/exception_handler.rb
@@ -0,0 +1,13 @@
+module ExceptionHandler
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      json_response({ message: e.message }, status: :not_found)
+    end
+
+    rescue_from ActiveRecord::RecordInvalid do |e|
+      json_response({ message: e.message }, status: :unprocessable_entity)
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,6 +28,8 @@ require 'database_cleaner'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -58,6 +60,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryGirl::Syntax::Methods
+  config.include RequestSpecHelper
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
