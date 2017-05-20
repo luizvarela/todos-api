79b8bce87f1150d5930256a73a5b864e1ed35adb
Luiz Varela <luiz.lvarela@gmail.com>
2017-05-20 14:04:38 -0300

    Add todo pagination

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'puma', '~> 3.0'
 gem 'bcrypt', '~> 3.1.7'
 gem 'jwt'
 gem 'active_model_serializers', '~> 0.10.0'
+gem 'will_paginate', '~> 3.1.0'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
--- app/controllers/v1/todos_controller.rb
+++ app/controllers/v1/todos_controller.rb
@@ -3,7 +3,7 @@ module V1
     before_action :set_todo, only: [:show, :update, :destroy]
 
     def index
-      @todos = current_user.todos
+      @todos = current_user.todos.paginate(page: params[:page], per_page: 20)
       json_response @todos
     end
 
@@ -35,5 +35,5 @@ module V1
     def set_todo
       @todo = Todo.find(params[:id])
     end
-  end  
+  end
 end
